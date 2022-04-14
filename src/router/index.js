0f4169802e97d5029d71d4b513f993791f8ff350
MatchaLove <1930399051@qq.com>
2022-04-14 12:41:33 +0800

    enter

--- src/router/index.js
+++ src/router/index.js
@@ -56,8 +56,107 @@ export const constantRoutes = [
       path: 'dashboard',
       name: 'Dashboard',
       component: () => import('@/views/dashboard/index'),
-      meta: { title: 'Dashboard', icon: 'dashboard' }
+      meta: { title: '个人中心', icon: 'dashboard' }
     }]
+  },
+  //信用评价模块的路由
+  {
+    path: '/credit',
+    component: Layout,
+    // redirect: '/credit',
+    name: 'Credit',
+    meta: { title: '信用评价', icon: 'el-icon-s-marketing' },
+    children: [
+      {
+        path: 'creditInquiry',
+        name: 'CreditInquiry',
+        component: () => import('@/views/credit/creditInquiry/index'),
+        meta: { title: '信用查询', icon: 'el-icon-search' }
+      },
+      {
+        path: 'evaluaCriteria',
+        name: 'EvaluaCriteria',
+        component: () => import('@/views/credit/evaluaCriteria/index'),
+        meta: { title: '评价标准', icon: 'el-icon-postcard' }
+      },
+      {
+        path: 'confirmationSubmit',
+        name: 'ConfirmationSubmit',
+        component: () => import('@/views/credit/confirmationSubmit/index'),
+        meta: { title: '提交证明', icon: 'el-icon-upload2' }
+      },
+      {
+        path: 'creditRepair',
+        name: 'CreditRepair',
+        component: () => import('@/views/credit/creditRepair/index'),
+        meta: { title: '信用修复', icon: 'el-icon-circle-plus-outline' }
+      },
+      {
+        path: 'creditGrading',
+        name: 'CreditGrading',
+        component: () => import('@/views/credit/creditGrading/index'),
+        meta: { title: '评价打分', icon: 'el-icon-s-order' }
+      }
+    ]
+  },
+
+  //容缺办理模块的路由
+  {
+    path: '/rongque',
+    component: Layout,
+    // redirect: '/credit',
+    name: 'Rongque',
+    meta: { title: '容缺办理', icon: 'el-icon-notebook-2' },
+    children: [
+      {
+        path: 'progressInquiry',
+        name: 'ProgressInquiry',
+        component: () => import('@/views/rongque/progressInquiry/index'),
+        meta: { title: '进度查询', icon: 'el-icon-search' }
+      },
+      {
+        path: 'approvalApply',
+        name: 'ApprovalApply',
+        component: () => import('@/views/rongque/approvalApply/index'),
+        meta: { title: '申请办理', icon: 'el-icon-edit-outline' }
+      },
+      {
+        path: 'materialSubmit',
+        name: 'MaterialSubmit',
+        component: () => import('@/views/rongque/materialSubmit/index'),
+        meta: { title: '提交材料', icon: 'el-icon-document-add' }
+      },
+      {
+        path: 'materialAudit',
+        name: 'MaterialAudit',
+        component: () => import('@/views/rongque/materialAudit/index'),
+        meta: { title: '材料审核', icon: 'el-icon-s-check' }
+      }
+    ]
+  },
+  //奖惩机制路由
+  {
+    path: '/rewardandpunish',
+    component: Layout,
+    // redirect: '/credit',
+    name: 'Rewardandpunish',
+    meta: { title: '奖惩机制', icon: 'el-icon-set-up' },
+    children: [
+      
+      {
+        path: 'blackListManage',
+        name: 'BlackListManage',
+        component: () => import('@/views/rewardandpunish/blackListManage/index'),
+        meta: { title: '黑名单管理', icon: 'el-icon-postcard' }
+      },
+      {
+        path: 'priorityManage',
+        name: 'PriorityManage',
+        component: () => import('@/views/rewardandpunish/priorityManage/index'),
+        meta: { title: '优先级管理', icon: 'el-icon-postcard' }
+      },
+      
+    ]
   },
 
   {
@@ -81,28 +180,7 @@ export const constantRoutes = [
       }
     ]
   },
-  //信用评价模块的路由
-  {
-    path: '/credit',
-    component: Layout,
-    // redirect: '/credit',
-    name: 'Credit',
-    meta: { title: '信用评价', icon: 'el-icon-s-help' },
-    children: [
-      {
-        path: 'creditinquiry',
-        name: 'CreditInquiry',
-        component: () => import('@/views/creditInquiry/index'),
-        meta: { title: '信用查询', icon: 'el-icon-search' }
-      },
-      {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
-      }
-    ]
-  },
+
   {
     path: '/form',
     component: Layout,
